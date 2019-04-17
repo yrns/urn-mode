d0b69080165849051dd0a14373b6795e0ca50c1f
Al McElrath <hello@yrns.org>
2019-04-17 12:39:26 -0700

    indent for with

--- urn-mode.el
+++ urn-mode.el
@@ -7,7 +7,15 @@
 ;;; Code:
 (require 'clojure-mode)
 
-(define-derived-mode urn-mode clojure-mode "Urn" "Major mode for Urn")
+(define-derived-mode urn-mode clojure-mode "Urn" "Major mode for Urn"
+  (setq-local clojure-align-binding-forms (append clojure-align-binding-forms '("with")))
+  (setq-local clojure--let-regexp
+	      "\(\\(when-let\\|if-let\\|let\\|with\\)\\(\\s-*\\|\\[\\)")
+
+  ;; Add "with" to other binding forms for indent. This affects
+  ;; clojure-mode too. FIX:
+  (put-clojure-indent 'with 1)
+  (font-lock-add-keywords nil '(("with" . font-lock-keyword-face))))
 
 (add-to-list 'auto-mode-alist (cons "\\.lisp\\'" 'urn-mode))
 
